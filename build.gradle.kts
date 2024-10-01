8726da71e714dd562e8b65c7196f98601f7d1c43
Andrew Golovashevich <landgrafhomyak@gmail.com>
2024-10-01 14:53:15 +0300

    Upgrading build script to Kotlin 2.0.20

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,42 +1,67 @@
+import org.jetbrains.kotlin.gradle.ExperimentalWasmDsl
+
 plugins {
-    kotlin("multiplatform") version "1.9.0"
-    `maven-publish`
+	kotlin("multiplatform") version "2.0.20"
+	`maven-publish`
 }
 
 group = "ru.landgrafhomyak"
-version = "1.0"
+version = "2.0"
 
 repositories {
-    mavenCentral()
+	mavenCentral()
 }
 
+@OptIn(ExperimentalWasmDsl::class)
 kotlin {
-    jvm {
-        jvmToolchain(8)
-    }
-    js {
-        browser()
-        nodejs()
-    }
+	jvmToolchain(8)
+	jvm {
+	}
+	js(IR) {
+		browser()
+		nodejs()
+	}
+	wasmWasi {
+		nodejs()
+	}
+	wasmJs {
+		nodejs()
+		browser()
+		d8()
+	}
 
-    macosArm64()
-    macosX64()
-    linuxArm64()
-    linuxX64()
-    mingwX64()
-    androidNativeArm32()
-    androidNativeArm64()
-    androidNativeX64()
-    androidNativeX86()
-    iosArm64()
-    iosSimulatorArm64()
-    iosX64()
-    ios()
-//    wasm()
-    tvos()
-    watchos()
+	macosArm64()
+	macosX64()
+	linuxArm64()
+	linuxX64()
+	mingwX64()
+	androidNativeArm32()
+	androidNativeArm64()
+	androidNativeX64()
+	androidNativeX86()
+	iosArm64()
+	iosSimulatorArm64()
+	iosX64()
+	tvosX64()
+	tvosArm64()
+	tvosSimulatorArm64()
+	watchosX64()
+	watchosArm32()
+	watchosArm64()
+	watchosDeviceArm64()
+	watchosSimulatorArm64()
 
-    sourceSets {
-        val commonMain by getting
-    }
+	sourceSets {
+		val commonMain by getting
+	}
 }
+
+
+
+
+
+
+
+
+
+
